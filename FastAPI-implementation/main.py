a0f3b8e71d4fe1bf93ea24b1eb2b77cc7dbb4a56
Jacky <Jayalago@charlotte.edu>
2025-10-22 19:58:56 -0400

    Added put function

--- FastAPI-implementation/main.py
+++ FastAPI-implementation/main.py
@@ -16,3 +16,7 @@ def root():
 @app.get("/items/{item_id}")
 def read_items(item_id: int, q: Union[str, None ] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
